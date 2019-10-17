7235d5ebeec192b7ffc37b7d88488063b1e00376
Ajay PATEL <apatel68@teaching.CS.McGill.CA>
2019-10-17 23:09:53 +0000

    working, writers obviously starving which is expected

--- RW.c
+++ RW.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
-
+#include <sys/time.h>
 
 #define WRITER_THREAD 10
-#define READER_THREAD 5
+#define READER_THREAD 500
 
 
 
-static sem_t rw_mutex;// = PTHREAD_MUTEX_INITIALIZER; //used by both reader and writer threads, mutual exclusion for writers
-static sem_t  mutex;// = PTHREAD_MUTEX_INITIALIZER; //ensure mutual exclusion when read_count is updated
+static sem_t rw_mutex; //used by both reader and writer threads, mutual exclusion for writers
+static sem_t  mutex; //ensure mutual exclusion when read_count is updated
 static int read_count = 0; //how many threads are reading the object
-
+static int Shared = 0;
 
 void *thread_function (void *arg) {
 	char *msg = arg;
@@ -25,12 +25,14 @@ void *writer(void *arg) {
 	do {
 		sem_wait(&rw_mutex);
 
-		printf("hello writer\n");
+		Shared+= 10;
+		printf("val: %d\n", Shared);
+		fflush(stdout);
 		
 		sem_post(&rw_mutex);
 
-
 	} while(1);
+
 	return NULL;
 
 
@@ -47,7 +49,9 @@ void *reader(void *arg) { //if continuous flow of readers, writers will starve
 		}
 		sem_post(&mutex);
 
-		printf("hello readers\n");
+		
+		printf("read: %d\n", Shared);
+		fflush(stdout);	
 		
 		sem_wait(&mutex);
 		read_count--;
@@ -83,11 +87,11 @@ int main (void) {
 	for (int i = 0; i < WRITER_THREAD; ++i) {
 		pthread_create(&thread_writer[i], NULL, writer, NULL);	
 	}
-	/*
+	
 	for (int y = 0; y < READER_THREAD; ++y) {
 		pthread_create(&thread_reader[y], NULL, reader, NULL);	
 	}
-	*/
+	
 
 	
 
@@ -95,34 +99,17 @@ int main (void) {
 	for (int i = 0; i < WRITER_THREAD; ++i) {
 		pthread_join(thread_writer[i], NULL);
 	}
-	/*
+	
 	for (int i = 0; i < READER_THREAD; ++i) {
 		pthread_join(thread_reader[i], NULL);
 	}
-	*/
+	
 	
 	return EXIT_SUCCESS;	
 
-/*
-	pthread_t t1, t2;
-	int s;
-	char *msg = "hello world";
 
 
 
-	s = pthread_create(&t1, NULL, thread_function, msg);
-	s |= pthread_create(&t2, NULL, thread_function, msg);
-
-	if (s != 0) exit(1);
-
-	s |= pthread_join(t1, NULL);
-	s |= pthread_join(t2, NULL);
-
-
-	if (s != 0) exit(1);
-	
-	return 0;
-	*/
 
 }
 
