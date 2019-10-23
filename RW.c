fa38eb4a414b3063e4028e2110d27e86891d566a
Ajay PATEL <apatel68@teaching.CS.McGill.CA>
2019-10-23 05:47:58 +0000

    timing done, starvation detected
    using timespec struct measuring time in microseconds (us)

--- RW.c
+++ RW.c
@@ -8,22 +8,36 @@
 #define WRITER_THREAD 10
 #define READER_THREAD 500
 
-#define READER_REPEAT 60
-#define WRITER_REPEAT 30
+#define MICRO 1E3
+
 
 static sem_t rw_mutex; //used by both reader and writer threads, mutual exclusion for writers
 static sem_t  mutex; //ensure mutual exclusion when read_count is updated
 static int read_count = 0; //how many threads are reading the object
 static int Shared = 0;
 
+double MINREADTIME = 1;
+double MAXREADTIME = 1;
+double MINWRITETIME = 1;
+double MAXWRITETIME = 1;
+
+double AVERAGEREAD = 0;
+double AVERAGEWRITE = 0;
 int readers;
 int writers;
+
 void *thread_function (void *arg) {
 	char *msg = arg;
 	printf("%s\n", msg);
 	return NULL;
 }
 
+int RNG() {
+	int RandomSleep = rand() % (101);
+	return RandomSleep * 1000;	
+
+
+}
 
 void *writer(void *arg) { 
 	
@@ -33,14 +47,35 @@ void *writer(void *arg) {
 	//	printf("attemptwriter: %d\n", number_attempt);
 
 	while (attempt < number_attempt) {
+		struct timespec tstart={0,0}, tend={0,0};
+		clock_gettime(CLOCK_MONOTONIC, &tstart);
+	
 		sem_wait(&rw_mutex);
 
+		clock_gettime(CLOCK_MONOTONIC, &tend);
+
+		double microsecond = (tend.tv_sec - tstart.tv_sec )+
+			       (tend.tv_nsec - tstart.tv_nsec)
+				 / MICRO;
+		AVERAGEWRITE += microsecond;
+
+
+		if (microsecond < MINWRITETIME) {
+			MINWRITETIME = microsecond;
+		}
+		if (microsecond > MAXWRITETIME) {
+			MAXWRITETIME = microsecond;
+		}
+		
+		//start critical section
 		Shared+= 10;
 		printf("val: %d\n", Shared);
 		fflush(stdout);
-		
+		//end critical section		
+
 		sem_post(&rw_mutex);
-		usleep(1000);
+
+		usleep(RNG());
 		attempt++;
 	}
 
@@ -54,24 +89,44 @@ void *reader(void *arg) { //if continuous flow of readers, writers will starve
 	int attempt = 0;
 	//printf("attempt: %d\n", number_attempt);
 	while (attempt < number_attempt) {
+		struct timespec tstart={0,0}, tend={0,0};
+		clock_gettime(CLOCK_MONOTONIC, &tstart);
+		
 		sem_wait(&mutex);
+
+		clock_gettime(CLOCK_MONOTONIC, &tend);
+
+		double microsecond = (tend.tv_sec - tstart.tv_sec )+
+			       (tend.tv_nsec - tstart.tv_nsec)
+				 / MICRO;
+
+		AVERAGEREAD += microsecond;
+		if (microsecond < MINREADTIME) {
+			MINREADTIME = microsecond;
+		}
+		if (microsecond > MAXREADTIME) {
+			MAXREADTIME = microsecond;
+		}
+
 		read_count++;
+
 		if (read_count == 1) {
 			sem_wait(&rw_mutex);
 		}
 		sem_post(&mutex);
 
-		
+		//start critical section 
 		printf("read: %d\n", Shared);
 		fflush(stdout);	
-		
+		//end critical section		
+
 		sem_wait(&mutex);
 		read_count--;
 		if (read_count == 0) {
 			sem_post(&rw_mutex);
 		}
 		sem_post(&mutex);
-		usleep(1000);
+		usleep(RNG());
 		attempt++;
 	} 
 
@@ -122,6 +177,16 @@ int main (int argc, char **argv) {
 	
 	sem_destroy(&mutex);
 	sem_destroy(&rw_mutex);	
+
+	printf("min read: %f\n", MINREADTIME);
+	printf("max read: %f\n", MAXREADTIME);
+	printf("min write: %f\n", MINWRITETIME);
+	printf("max write: %f\n", MAXWRITETIME);
+	printf("average read: %f\n", AVERAGEREAD/(READER_THREAD*reader_repeat) );
+	printf("average write: %f\n", AVERAGEWRITE/(WRITER_THREAD*writer_repeat) );
+
+
+
 	return EXIT_SUCCESS;	
 
 
